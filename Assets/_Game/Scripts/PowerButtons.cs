db0295d24ebce83bd135a946f8cf90ef30e385b6
morg198 <morg198@yahoo.com>
2015-03-16 10:38:23 -0700

    Deactivation
    Needs testing

--- Assets/_Game/Scripts/PowerButtons.cs
+++ Assets/_Game/Scripts/PowerButtons.cs
@@ -11,6 +11,9 @@ public class PowerButtons : TouchManager {
 		public ParticleSystem Explosion1;
 		public AudioClip Meow;
 
+		public int deactivateTime;
+		Weapon script;
+
 	// Update is called once per frame
 	void Update () {
 	
@@ -20,29 +23,41 @@ public class PowerButtons : TouchManager {
 		
 
 
+	void Start()
+	{
+			deactivateTime = 5;
+	}
+
 
 	void OnFirstTouch ()
 	{
 		if (buttonType == button.ButtonOne) {
 				Explosion1.Play ();
 		} else if (buttonType == button.ButtonTwo) {
-				Weapon script;
+
 				script = weaponThing.GetComponent<Weapon>();
 				script.enabled = true;
+				Invoke("disable", deactivateTime);
 		} else if (buttonType == button.ButtonThree) {
 
 		}
 	}
 
+
+
+	void disable()
+	{
+			script.enabled = false;
+	}
 	//just a copy of the OnFirstTouch settings so that it will register two touches
 	void OnSecondTouch ()
 	{
 		if (buttonType == button.ButtonOne) {
 				Explosion1.Play ();
 		} else if (buttonType == button.ButtonTwo) {
-				Weapon script;
 				script = weaponThing.GetComponent<Weapon>();
 				script.enabled = true;
+				Invoke("disable", deactivateTime);
 		} else if (buttonType == button.ButtonThree) {
 
 			}
