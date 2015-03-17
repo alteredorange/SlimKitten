94eccc9f9a9b6d79d3aed4f72250d606ee214ecc
Kyle Fitzgerald <alteredorange@gmail.com>
2015-03-16 22:23:59 -0600

    Barrier Powerup Now Disappears After 5 Seconds

---
+++ Assets/DestroyBarrier.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBarrier : MonoBehaviour {
+
+	public int barrierTime = 5;
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, barrierTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
