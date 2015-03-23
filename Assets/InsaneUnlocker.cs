a427e7578373e6253c3b31133aadb65eaa89a116
Kyle Fitzgerald <alteredorange@gmail.com>
2015-03-23 14:16:18 -0600

    Free Coin Spin

---
+++ Assets/InsaneUnlocker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InsaneUnlocker : MonoBehaviour {
+
+	public GameObject insaneLevelButton;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+		insaneLevelButton.transform.Rotate (0, 0.5f, 0);
+
+	}
+}
